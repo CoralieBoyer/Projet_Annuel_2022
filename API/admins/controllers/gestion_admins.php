6b0d4b6c36772600542bed09b1f4a88ac2a6a51f
Coralie  Boyer <cocolad@Coralies-MacBook-Pro.local>
2022-05-08 13:37:13 +0200

    password admin/gestion-admin

--- API/admins/controllers/gestion_admins.php
+++ API/admins/controllers/gestion_admins.php
@@ -18,12 +18,15 @@ class Admins{
       echo json_encode('Ajout effectué.');
     }
 
-    if($_POST['action'] == "modify"){
+    elseif($_POST['action'] == "modify"){
       UserModel::modify($_POST['name'], $_POST['email'], $_POST['id_user']);
       AdminModel::modify($_POST['firstname'], $_POST['id']);
-      echo json_encode($_POST['email']);
+      echo json_encode("Modification effectuée.");
     }
-    if($_POST['action'] == "delete"){
+    elseif($_POST['action'] == "modifyPassword"){
+      UserModel::modifyPasswd($_POST['newPassword'], $_POST['id_user']);
+    }
+    elseif($_POST['action'] == "delete"){
       UserModel::delete($_POST['id_user']);
       AdminModel::delete($_POST['id']);
       echo json_encode('Suppression effectuée.');
--- WebApp/src/app/admins/gestion-admins/gestion-admins.component.ts
+++ WebApp/src/app/admins/gestion-admins/gestion-admins.component.ts
@@ -7,7 +7,7 @@ import { ApiConnexionForAdmin } from '../services/api.service';
   styleUrls: ['./gestion-admins.component.css']
 })
 export class GestionAdminsComponent implements OnInit {
-  attributsAdmin!: {email: string, firstname: string, id: string, id_user: string, label_role: string, name: string, password:string}[];
+  attributsAdmin!: {email: string, firstname: string, id: string, id_user: string, label_role: string, name: string, newPassword:string}[];
   NewAttributsAdmin = {email: "", firstname: "", name: "", password:""};
   headerTab = [ "ID", "Nom", "Prénom", "Email", "Mot de passe", "Actions"];
 
@@ -53,12 +53,11 @@ export class GestionAdminsComponent implements OnInit {
       });
   }
 
-  modify(id_user: string, name : string, password : string, firstname: string, email: string, id: string){
+  modify(id_user: string, name : string, newpassword: string, firstname: string, email: string, id: string){
     const formData: FormData = new FormData();
     formData.append("action", "modify");
     formData.append("id_user", id_user);
     formData.append("name", name);
-    formData.append("password", password);
     formData.append("firstname", firstname);
     formData.append("email", email);
     formData.append("id", id);
@@ -69,5 +68,18 @@ export class GestionAdminsComponent implements OnInit {
       err=>{
         console.log(err);
       });
+    if(newpassword != ""){
+      const formData: FormData = new FormData();
+      formData.append("action", "modifyPassword");
+      formData.append("id_user", id_user);
+      formData.append("newPassword", newpassword);
+      this.apiConnexionForAdmin.GestionAdminsPostService(formData).subscribe(res=>{
+          this.ngOnInit();
+        },
+        err=>{
+          console.log(err);
+        });
+    }
   }
+
 }
